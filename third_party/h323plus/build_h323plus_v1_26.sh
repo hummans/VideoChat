f6fc2d57c6a59b65266d34ba88c76303eb84291e
Chengmc <Martin.Cheng@rock-chips.com>
2017-08-31 00:30:03 +0800

    Optimize Scripts to build h323plus and ptlib

--- third_party/h323plus/build_h323plus_v1_26.sh
+++ third_party/h323plus/build_h323plus_v1_26.sh
@@ -1,2 +1,12 @@
+#STEP 1: download stable h323plus
+wget http://www.h323plus.org/source/download/h323plus-v1_26_5.tar.gz
 md5sum h323plus-v1_26_5.tar.gz
+
+#STEP 2: release h323plus
+tar -zxf h323plus-v1_26_5.tar.gz
 cd h323plus-1.26/
+
+#STEP 3: config and build
+./configure
+make; make install
+
--- third_party/h323plus/build_ptlib_v2_12.sh
+++ third_party/h323plus/build_ptlib_v2_12.sh
@@ -1,3 +1,11 @@
+#STEP 1: download stable ptlib
+wget http://www.h323plus.org/source/download/ptlib-2.12.8.tar.bz2
 md5sum ptlib-2.12.8.tar.bz2
+
+#STEP 2: Release PTLib
 tar -xjf ptlib-2.12.8.tar.bz2
 cd ptlib-2.12.8/
+
+#STEP 3: Config and Build
+./configure
+make; make install
