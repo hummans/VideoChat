27a3aa43c79723df952dc65c119348a805cb5d85
Chengmc <Martin.Cheng@rock-chips.com>
2017-08-31 09:22:18 +0800

    Optimize Build Scripts for third_party/h323plus

--- third_party/h323plus/build_h323plus_v1_26.sh
+++ third_party/h323plus/build_h323plus_v1_26.sh
@@ -1,12 +1,23 @@
 #STEP 1: download stable h323plus
-wget http://www.h323plus.org/source/download/h323plus-v1_26_5.tar.gz
-md5sum h323plus-v1_26_5.tar.gz
-
+H323PLUS_TAR=h323plus-v1_26_5.tar.gz
+echo "STEP 1: Download h323plus.org/source/download/${H323PLUS_TAR}"
+if [ ! -f ${H323PLUS_TAR} ]; then 
+wget http://www.h323plus.org/source/download/${H323PLUS_TAR}
+md5sum ${H323PLUS_TAR}
 #STEP 2: release h323plus
-tar -zxf h323plus-v1_26_5.tar.gz
-cd h323plus-1.26/
+tar -zxf ${H323PLUS_TAR}
+fi
+
+#STEP 2: Release h323plus
+H323PLUS_ROOT=h323plus-1.26
+echo "STEP 2: Release H323plus Package"
+if [ ! -d ${H323PLUS_TAR} ]; then 
+tar -zxf ${H323PLUS_TAR}
+fi
 
 #STEP 3: config and build
+echo "STEP 3: Config and Build ${H323PLUS_ROOT}"
+cd ${H323PLUS_ROOT}
 ./configure
 make; make install
 
--- third_party/h323plus/build_ptlib_v2_12.sh
+++ third_party/h323plus/build_ptlib_v2_12.sh
@@ -1,11 +1,20 @@
 #STEP 1: download stable ptlib
-wget http://www.h323plus.org/source/download/ptlib-2.12.8.tar.bz2
-md5sum ptlib-2.12.8.tar.bz2
+PTLIB_TAR=ptlib-2.12.8.tar.bz2
+echo "STEP 1: Download http://www.h323plus.org/source/download/${PTLIB_TAR}"
+if [ ! -f ${PTLIB_TAR} ]; then 
+wget http://www.h323plus.org/source/download/${PTLIB_TAR}
+fi
+md5sum ${PTLIB_TAR}
 
 #STEP 2: Release PTLib
-tar -xjf ptlib-2.12.8.tar.bz2
-cd ptlib-2.12.8/
+echo "STEP 2: Release Software Package:${PTLIB_TAR}"
+PTLIB_ROOT=ptlib-2.12.8
+if [ ! -d ${PTLIB_ROOT} ]; then
+tar -xjf ${PTLIB_TAR}
+fi
 
 #STEP 3: Config and Build
+echo "STEP 3: Config and Build"
+cd ${PTLIB_ROOT}
 ./configure
 make; make install
