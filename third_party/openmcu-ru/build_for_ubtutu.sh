313e8a580db23510eb2f048625a5f4fad80a2fd7
Chengmc <Martin.Cheng@rock-chips.com>
2017-08-31 08:51:55 +0800

    Optimize Script for openmcu-ru/build_for_ubtutu.sh

--- third_party/openmcu-ru/build_for_ubtutu.sh
+++ third_party/openmcu-ru/build_for_ubtutu.sh
@@ -1,4 +1,4 @@
-#STEP 1: Install dependencies
+#STEP 1: Install dependencies(Need Root Permissions)
 apt-get install \
         build-essential flex bison autoconf automake pkg-config \
         libfreetype6-dev libjpeg62-dev libsofia-sip-ua-dev \
@@ -6,12 +6,21 @@ apt-get install \
         libavutil-dev libsamplerate0-dev libopus-dev
 
 #STEOP 2: Clone Source Codes
-git clone git://github.com/muggot/openmcu.git ./openmcu-ru-4.x
-cd ./openmcu-ru-4.x
+OPENMCU_RU_ROOT=openmcu-ru
+if [ ! -d ${OPENMCU_RU_ROOT} ]; then 
+git clone git://github.com/muggot/openmcu.git ${OPENMCU_RU_ROOT}
+fi
+cd ./${OPENMCU_RU_ROOT}
+git branch -a
 
 #STEP 3: Config and Install
+OPENMCU_RU_BIN=/opt/openmcu-ru/bin/openmcu-ru
+if [ ! -f ${OPENMCU_RU_BIN} ]; then
+echo "Autogen and Config ${OPENMCU_RU_ROOT} First"
 ./autogen.sh
 ./configure --disable-g7221   #Fail to config and compile g722-1 audio plugin
+echo "autogen and config"
+fi
 make; make install
 
 #STEP 4: Run OpenMCU-ru
