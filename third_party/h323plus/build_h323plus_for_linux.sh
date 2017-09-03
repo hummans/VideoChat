b50938916cd396dd8cb441c17d6bcc137ded96f6
martin.ming <shareviews@sina.com>
2017-09-03 22:53:22 +0800

    Optimize Scripts for h323plus-v1.26 ptlib-v2.12
    Building Pass/OK in ubuntu
    
    Signed-off-by: martin.ming <shareviews@sina.com>

--- third_party/h323plus/build_h323plus_for_linux.sh
+++ third_party/h323plus/build_h323plus_for_linux.sh
@@ -1,28 +1,33 @@
-#STEP 1: download stable h323plus
+H323PLUS_ROOT=h323plus
 H323PLUS_TAR=h323plus-v1_26_5.tar.gz
+
+#STEP 1: download stable h323plus
 echo "STEP 1: Download h323plus.org/source/download/${H323PLUS_TAR}"
 if [ ! -f ${H323PLUS_TAR} ]; then 
 wget http://www.h323plus.org/source/download/${H323PLUS_TAR}
 md5sum ${H323PLUS_TAR}
-#STEP 2: release h323plus
-tar -zxf ${H323PLUS_TAR}
 fi
 
 #STEP 2: Release h323plus
-H323PLUS_ROOT=h323plus-1.26
 echo "STEP 2: Release H323plus Package"
-if [ ! -d ${H323PLUS_TAR} ]; then 
+if [ ! -d ${H323PLUS_ROOT} ]; then 
 tar -zxf ${H323PLUS_TAR}
+mv h323plus-1.26 ${H323PLUS_ROOT}
 fi
 
 #STEP 3: config and build
 echo "STEP 3: Config and Build ${H323PLUS_ROOT}"
 cd ${H323PLUS_ROOT}
-ls -al ${PWD}/../../built_libs_linux/ptlib/share/ptlib/make
 export PTLIB_CFLAGS=' -DP_64BIT -DPTRACING=2 -D_REENTRANT -fno-exceptions '
+export PTLIBDIR=${PWD}/../ptlib
+#export PTLIB_LIBS=${PWD}/../../built_libs_linux/ptlib
 export PKG_CONFIG_PATH=${PWD}/../../built_libs_linux/ptlib/lib/pkgconfig
-export PTLIBDIR=${PWD}/../../built_libs_linux/ptlib/share/ptlib
-echo ${PT_LIBDIR}
-./configure
-make; make install
+#inport PTLib Path(ld libpt.so) for H323plus
+export LIBRARY_PATH=${PWD}/../../built_libs_linux/ptlib/lib
+./configure --prefix=${PWD}/../../built_libs_linux/h323plus
+make -j16;
+make install
 
+echo "Have a look to ptlib and h323plus"
+ls -al ${PWD}/../../built_libs_linux/ptlib
+ls -al ${PWD}/../../built_libs_linux/h323plus
--- third_party/h323plus/build_ptlib_for_linux.sh
+++ third_party/h323plus/build_ptlib_for_linux.sh
@@ -1,5 +1,7 @@
-#STEP 1: download stable ptlib
+PTLIB_ROOT=ptlib
 PTLIB_TAR=ptlib-2.12.8.tar.bz2
+
+#STEP 1: download stable ptlib
 echo "STEP 1: Download http://www.h323plus.org/source/download/${PTLIB_TAR}"
 if [ ! -f ${PTLIB_TAR} ]; then 
 wget http://www.h323plus.org/source/download/${PTLIB_TAR}
@@ -8,13 +10,14 @@ md5sum ${PTLIB_TAR}
 
 #STEP 2: Release PTLib
 echo "STEP 2: Release Software Package:${PTLIB_TAR}"
-PTLIB_ROOT=ptlib-2.12.8
 if [ ! -d ${PTLIB_ROOT} ]; then
 tar -xjf ${PTLIB_TAR}
+mv ${PTLIB_ROOT}-2.12.8 ${PTLIB_ROOT}
 fi
 
 #STEP 3: Config and Build
 echo "STEP 3: Config and Build"
 cd ${PTLIB_ROOT}
 ./configure --prefix=${PWD}/../../built_libs_linux/ptlib
-make; make install
+make -j16;
+make install
